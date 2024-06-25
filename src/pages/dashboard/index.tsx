825ae2d55f2b47254aabeb84390d40ca1e1f3357
albertpanesse <albert.panesse@gmail.com>
2024-06-25 10:54:25 +0700

    ok

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,7 +4,7 @@ const Dashboard: React.FC = () => {
     return (
         <div>
             <h2>Dashboard</h2>
-            <p>Welcome to your dashboard. Here, you can view and manage your account information.</p>
+            <p>Welcomes to your dashboard. Here, you can view and manage your account information.</p>
             {/* Add dashboard content and functionality here */}
         </div>
     );
